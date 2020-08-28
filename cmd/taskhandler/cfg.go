3900a32bfb4125430a9ca383cbaa9afd8883b323
Mads Kalør <mads@kaloer.com>
2020-08-28 21:15:43 +0200

    Use env vars as configs

--- cmd/taskhandler/cfg.go
+++ cmd/taskhandler/cfg.go
@@ -9,10 +9,15 @@ func SetConfig() {
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
 	viper.SetConfigType("yaml")
+	viper.SetEnvPrefix("tfsc")
 
 	err := viper.ReadInConfig() // Find and read the config file
 	if err != nil {             // Handle errors reading the config file
-		log.WithError(err).Panic("Could not read config file")
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Info("No config file found. Reading from env vars")
+		} else {
+			log.WithError(err).Panic("Could not read config file")
+		}
 	}
 
 }
